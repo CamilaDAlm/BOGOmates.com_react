4b0f48ade0ed16659875422fc73ed73192827eef
CelesIso <upsilontr2018@gmail.com>
2024-02-01 19:35:06 +0100

    update

---
+++ bogomates_fakedoor/src/bogoapp.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Counter from "./counter";
+
+export class Bogoapp extends React.Component {
+
+  render(){
+
+    return(
+
+    <div>
+    <h2>PAGINA DE EJEMPLO</h2>
+    <p>holaaaa</p>
+    <Counter></Counter>
+   </div>
+   )
+
+  };
+
+}
